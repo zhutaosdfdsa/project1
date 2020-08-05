60d46532de47cc9526265e357fbe7b34f89eccc0
涛哥 <784885681@qq.com>
2020-08-05 15:22:47 +0800

    测试代码2

--- git_Test/src/qdgr/testDemo.java
+++ git_Test/src/qdgr/testDemo.java
@@ -9,5 +9,7 @@ package qdgr;
 public class testDemo {
     public static void main(String[] args) {
         System.out.println("Hello Wordl!");
+        System.out.println("Hello Wordl!");
+        System.out.println("Hello Wordl!");
     }
 }
