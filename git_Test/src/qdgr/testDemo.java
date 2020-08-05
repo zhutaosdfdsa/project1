709d5d6294115fc901a4711ec22abb7444bf213b
涛哥 <784885681@qq.com>
2020-08-05 15:20:51 +0800

    测试代码

---
+++ git_Test/src/qdgr/testDemo.java
@@ -0,0 +1,13 @@
+package qdgr;
+
+/**
+ * @Program: git_Test
+ * @Description: 1
+ * @Author: ZT
+ * @Date: 2020-08-05 15:18
+ **/
+public class testDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl!");
+    }
+}
